2f26fe8e8cbe4c2774e0a6a3cf21bd1b3e100aab
Nicole Hollant <nicolehollant@gmail.com>
2023-06-30 14:32:20 -0400

    lol

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,17 @@ export default defineNuxtConfig({
   modules: ['@nuxthq/ui'],
   srcDir: './src',
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'iHateMFA',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Silly way to get around MFA. I am begging you to not use this.',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+    },
+  },
 })
